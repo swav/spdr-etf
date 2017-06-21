73c70cb5ee1b14efa7090d9d6afd7f124230aadf
swav <swavrybak@gmail.com>
2017-06-20 21:50:22 -0700

    add process function to process all defined symbosl

--- spdr.py
+++ spdr.py
@@ -33,17 +33,40 @@ def summarize(map_list):
         (dates6, rs6) = map_list.get(6)[12-6+i]
         (dates3, rs3) = map_list.get(3)[12-3+i]
         (dates1, rs1) = map_list.get(1)[12-1+i]
-        print("dates12:", dates12, "rs12=", rs12)
-        print("dates6:", dates6, "rs6=", rs6)
-        print("dates3:", dates3, "rs3=", rs3)
-        print("dates1:", dates1, "rs1=", rs1)
+##        print("dates12:", dates12, "rs12=", rs12)
+##        print("dates6:", dates6, "rs6=", rs6)
+##        print("dates3:", dates3, "rs3=", rs3)
+##        print("dates1:", dates1, "rs1=", rs1)
         result.append((dates12[0], rs12+rs6+rs3+rs1))
     return result
-    
-if __name__ == '__main__':
-    xlp = calc_for('XLP', '/Users/swav/dev/projects/python/spdr-etf/data/XLP.csv')
-    xlp_summary = summarize(xlp)
 
+spdrs = ['XLB', 'XLE', 'XLF', 'XLI', 'XLK', 'XLP', 'XLRE', 'XLV', 'XLY', 'XLU']
+
+def process(symbols):
+    results = {}
+    for s in symbols:
+        path = '/Users/swav/dev/projects/python/spdr-etf/data/{}.csv'.format(s)
+        print('loading: {}'.format(path))
+        s_calc = calc_for(s, path)
+        results[s] = summarize(s_calc)
+
+    return results
+
+
+if __name__ == '__main__':
+    r = process(spdrs)
+##    xlp = calc_for('XLP', '/Users/swav/dev/projects/python/spdr-etf/data/XLP.csv')
+##    xlp_summary = summarize(xlp)
+##
+##    xly = calc_for('XLY', '/Users/swav/dev/projects/python/spdr-etf/data/XLY.csv')
+##    xly_summary = summarize(xly)
+##
+##    xle = calc_for('XLE', '/Users/swav/dev/projects/python/spdr-etf/data/XLY.csv')
+##    xle_summary = summarize(xle)
+
+        
+        
+    
 ##>>> xlp.get(12)[0]
 ##(((2000, 1, 1), (1999, 1, 1)), -0.14079250975682303)
 ##>>> xlp.get(6)[0]
